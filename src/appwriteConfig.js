94ef60c1b5f28e283dae78ebab3da83dfa278122
Collins <149684399+coll254-strath@users.noreply.github.com>
2024-06-03 15:52:49 +0100

    Update appwriteConfig.js

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -2,7 +2,7 @@
 import { Client, Account, Databases } from 'appwrite';
 
 export const API_ENDPOINT = 'https://cloud.appwrite.io'
-export const PROJECT_ID = "66219f9e0ac*****cbeb"
+export const PROJECT_ID = "****19f9e0ac*****cbeb"
 export const DATABASE_ID = "production"
 export const COLLECTION_ID_MESSAGES = "messages"
     
@@ -10,7 +10,7 @@ const client = new Client();
 
 client
    .setEndpoint('https://cloud.appwrite.io/v1')
-   .setProject('66219f9e0*****cbeb');
+   .setProject('662*****19f9*0*****cbeb');
    
 export const account = new Account(client);
 export const databases = new Databases(client)
@@ -20,7 +20,7 @@ export default client;
 
 //import { Client, Databases } from 'appwrite';
 
-//export const PROJECT_ID = '66219f9e0acc3' ;
+//export const PROJECT_ID = '66****9f9*0a***c3' ;
 //export const DATABASE_ID='viter_1';
 //export const COLLECTION_ID_MESSAGES='messages_table1';
 
